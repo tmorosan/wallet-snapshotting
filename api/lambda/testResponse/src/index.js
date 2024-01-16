d1b3c1b9e5b45c2ca63ff83504ce89fd9b9099a6
tmorosan <tudormorosan1@gmail.com>
2024-01-16 17:20:55 +0200

    feat: add dynamo example

--- api/lambda/testResponse/src/index.js
+++ api/lambda/testResponse/src/index.js
@@ -1,14 +1,45 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", { value: true });
-module.exports.handler = async (event) => {
-    console.log("Received event:", JSON.stringify(event, null, 2));
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            message: "Hello from Lambda!",
-        }),
-    };
+exports.handler = void 0;
+const client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
+const lib_dynamodb_1 = require("@aws-sdk/lib-dynamodb");
+const crypto_1 = require("crypto");
+const safeGetEnvVar = (envName) => {
+    const value = process.env?.[envName];
+    if (!value) {
+        throw new Error(`${envName} is undefined`);
+    }
+    return value;
 };
+const dynamodb = new client_dynamodb_1.DynamoDBClient({
+    region: safeGetEnvVar("REGION"),
+});
+const client = lib_dynamodb_1.DynamoDBDocument.from(dynamodb);
+async function handler(event) {
+    try {
+        const now = new Date().toISOString();
+        const putCommand = {
+            TableName: safeGetEnvVar("COLLECTION_TABLE"),
+            Item: {
+                Id: (0, crypto_1.randomUUID)(),
+                Name: "test",
+                createdAt: now,
+                updatedAt: now,
+            },
+        };
+        const result = await client.put(putCommand);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result, null, "\t"),
+        };
+    }
+    catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e, null, "\t"),
+        };
+    }
+}
+exports.handler = handler;
 //# sourceMappingURL=index.js.map
--- api/lambda/testResponse/src/index.ts
+++ api/lambda/testResponse/src/index.ts
@@ -1,11 +1,43 @@
-module.exports.handler = async (event: any) => {
-  console.log("Received event:", JSON.stringify(event, null, 2));
+import type {APIGatewayProxyEventV2, APIGatewayProxyResultV2} from "aws-lambda";
+import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import {DynamoDBDocument} from "@aws-sdk/lib-dynamodb";
+import {randomUUID} from "crypto";
 
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      message: "Hello from Lambda!",
-    }),
-  };
+const safeGetEnvVar = (envName: string) => {
+    const value = process.env?.[envName];
+    if (!value) {
+        throw new Error(`${envName} is undefined`);
+    }
+    return value;
 };
+
+const dynamodb = new DynamoDBClient({
+    region: safeGetEnvVar("REGION"),
+});
+
+const client = DynamoDBDocument.from(dynamodb);
+
+export async function handler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
+    try {
+        const now = new Date().toISOString();
+        const putCommand = {
+            TableName: safeGetEnvVar("COLLECTION_TABLE"),
+            Item: {
+                Id: randomUUID(),
+                Name: "test",
+                createdAt: now,
+                updatedAt: now,
+            },
+        }
+        const result = await client.put(putCommand);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result, null, "\t"),
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e, null, "\t"),
+        };
+    }
+}
